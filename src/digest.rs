e3a28ebaeaae610c98dcb55ca0065d725b5ad62c
Sathish <tsatiz@gmail.com>
2018-05-09 15:39:25 +0530

    Added thread in comparison as well

--- src/digest.rs
+++ src/digest.rs
@@ -1,13 +1,13 @@
 use csv;
+use csv::StringRecord;
 use std;
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::hash::BuildHasherDefault;
 use std::hash::Hasher;
-use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use twox_hash::XxHash;
-use csv::StringRecord;
 
 pub fn digest(mut reader: csv::Reader<std::fs::File>, mut another_reader: csv::Reader<std::fs::File>) {
     fn get_hash(input: &[u8]) -> u64 {
@@ -26,14 +26,10 @@ pub fn digest(mut reader: csv::Reader<std::fs::File>, mut another_reader: csv::R
     });
 
     reader.records().for_each(|record| {
-        let hash_sender_clone = hash_sender.clone();
         match record {
             Ok(string_record) => {
-                let
-                record_clone = string_record.clone();
-                if let Some(record) = record_clone.get(0) {
-                    hash_sender_clone.send((String::from(record), string_record)).unwrap();
-                }
+                let record = string_record.get(0).unwrap().to_owned();
+                hash_sender.send((String::from(record), string_record)).unwrap();
             }
             _ => ()
         };
@@ -42,27 +38,31 @@ pub fn digest(mut reader: csv::Reader<std::fs::File>, mut another_reader: csv::R
     drop(hash_sender);
     hash = hash_handler.join().unwrap();
 
+    let (hash_sender, hash_receiver): (Sender<(String, StringRecord)>, Receiver<(String, StringRecord)>) = channel();
+
+    let hash_handler = thread::spawn(move || {
+        for (key, value) in hash_receiver.iter() {
+            match hash.get(&key) {
+                Some(existing_record) => match existing_record.eq(&get_hash(value.as_slice().as_bytes())) {
+                    true => (),
+                    false => println!("Changed {:?}", value)
+                },
+                None => println!("Addition {:?}", value)
+            }
+        };
+    });
+
     for record in another_reader.records() {
         match record {
             Ok(string_record) => {
-                let record_clone = string_record.clone();
-                if let Some(record) = record_clone.get(0) {
-                    match hash.get(&String::from(record)) {
-                        Some(record_value) => {
-                            match get_hash(string_record.as_slice().as_bytes()).cmp(record_value) {
-                                Ordering::Equal => (),
-                                _ => println!("Changed {:?}", string_record)
-                            }
-                        }
-                        None => {
-                            println!("Addition {:?}", string_record);
-                        }
-                    }
-                }
+                let record = string_record.get(0).unwrap().to_owned();
+                hash_sender.send((String::from(record), string_record)).unwrap();
             }
             Err(_e) => {}
         }
     }
+    drop(hash_sender);
+    hash_handler.join().unwrap();
 }
 
 mod test {}
